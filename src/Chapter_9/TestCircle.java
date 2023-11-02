a13ded4b38d1f1d868f7301c8633dd2286f8df6d
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-02 09:02:38 -0700

    fc

---
+++ src/Chapter_9/TestCircle.java
@@ -0,0 +1,15 @@
+package Chapter_9;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        System.out.println("the area of the " + circle1.radius + "is" + circle1.getArea());
+        Circle circle2 = new Circle(25);
+        System.out.println(+circle2.radius+ "is" + circle2.getArea());
+        Circle circle3 = new Circle(125);
+        System.out.println(+circle3.radius + "is" + circle3.getArea());
+        circle2.radius=100;
+        System.out.println(circle2.radius + "is" + circle2.getArea() );
+
+    }
+}
