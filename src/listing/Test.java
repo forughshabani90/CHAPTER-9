9ce3d67516bdc8acef48979b24e4862e546c61e7
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-08 11:57:32 -0800

    sc

--- src/listing/Test.java
+++ src/listing/Test.java
@@ -1,19 +1,19 @@
 package listing;
 
 public class Test {
-    int i=0;
-    int j=0;
+    int i = 0;
+    int j = 0;
 
     public static void main(String[] args) {
-        int i=2;
-        int k=3;
+        int i = 2;
+        int k = 3;
 
-    int j=3;
-        System.out.println("i+j is" + i+j);
-        k=i+j;
+        int j = 3;
+        System.out.println("i+j is" + i + j);
+        k = i + j;
         System.out.println("k is" + k);
         System.out.println("j is" + j);
-}
+    }
 
 
 }
