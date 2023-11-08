610b1e8f6d50abd9e6f52bb2e697232119232993
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-08 11:56:19 -0800

    fc

--- src/listing/Test.java
+++ src/listing/Test.java
@@ -7,7 +7,13 @@ public class Test {
     public static void main(String[] args) {
         int i=2;
         int k=3;
-    }
-    j=3;
+
+    int j=3;
+        System.out.println("i+j is" + i+j);
+        k=i+j;
+        System.out.println("k is" + k);
+        System.out.println("j is" + j);
+}
+
 
 }
