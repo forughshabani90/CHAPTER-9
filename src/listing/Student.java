5d14d944afad3a7edcad1b4c4e64407fd64a5033
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-08 11:50:20 -0800

    sc

--- src/listing/Student.java
+++ src/listing/Student.java
@@ -4,22 +4,25 @@ public class Student {
     private int id;
     private String name;
     private java.util.Date dateCreated;
-    public Student(int ssn , String newName){
-        id=ssn;
-        name=newName;
-        dateCreated= new java.util.Date();
+
+    public Student(int ssn, String newName) {
+        id = ssn;
+        name = newName;
+        dateCreated = new java.util.Date();
 
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public java.util.Date getDateCreated(){
+
+    public java.util.Date getDateCreated() {
         return dateCreated;
     }
-
 
 
 }
---
+++ src/listing/Test.java
@@ -0,0 +1,13 @@
+package listing;
+
+public class Test {
+    int i=0;
+    int j=0;
+
+    public static void main(String[] args) {
+        int i=2;
+        int k=3;
+    }
+    j=3;
+
+}
