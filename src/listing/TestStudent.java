9f856f9d812f414218bca44faa2bdd554e27471a
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-26 08:29:10 -0800

    fc

---
+++ src/listing/TestStudent.java
@@ -0,0 +1,9 @@
+package listing;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student=new Student(12 , "ali");
+        System.out.println("name? " + student.getName());
+        System.out.println("id " + student.getId());
+    }
+}
